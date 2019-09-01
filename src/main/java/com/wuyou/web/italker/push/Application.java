74a845930f92892681314f80ed27baafb5a99a50
wuyou1998 <1020131041@qq.com>
2019-09-01 23:51:40 +0800

    依赖配置

---
+++ src/main/java/com/wuyou/web/italker/push/Application.java
@@ -0,0 +1,10 @@
+package com.wuyou.web.italker.push;
+
+/**
+ * 名称: ITalkerServer.com.wuyou.web.italker.push.Application
+ * 用户: _VIEW
+ * 时间: 2019/9/1,23:42
+ * 描述: 入口
+ */
+public class Application {
+}
